80a6a44cc1b5f2cd83932adf1267b6eccef39927
Robin Lundell <mailtome@robinlundell.se>
2019-02-26 14:12:45 +0100

    lite pill

---
+++ tdde42-master/js/java.js
@@ -0,0 +1,25 @@
+
+/*
+function toggleButtons() {
+  var buttons = document.getElementById("buttons");
+  var toggleButton = document.getElementById("toggleBtn");
+  if (buttons.style.display == "none") {
+    buttons.style.display = "flex";
+    toggleButton.innerHTML = "🐵"
+  } else {
+    buttons.style.display = "none";
+    toggleButton.innerHTML = "🙈"
+  }
+}
+*/
+function toggleButtons() {
+  var buttons = document.getElementById("buttons");
+  var toggleButton = document.getElementById("toggleBtn");
+  if (buttons.style.display == "flex") {
+    buttons.style.display = "none";
+    toggleButton.innerHTML = "<img src='images/menubar.svg' width='50px'>"
+  } else {
+    buttons.style.display = "flex";
+    toggleButton.innerHTML = "<img src='images/menubar.svg' width='50px' style='transform:rotate(90deg)'>"
+  }
+}
