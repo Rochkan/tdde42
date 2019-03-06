7b44c45e7ece145311d3677a0506570dae14daa0
Robin Lundell <mailtome@robinlundell.se>
2019-03-06 14:19:34 +0100

    eget fix

--- js/collaps.js
+++ js/collaps.js
@@ -1,5 +1,51 @@
 
-var coll = document.getElementsByClassName("collapsible");
+var coll = document.getElementsByClassName("collapsibleovr");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
+
+
+var coll = document.getElementsByClassName("collapsiblenat");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
+
+var coll = document.getElementsByClassName("collapsibleeko");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
+
+var coll = document.getElementsByClassName("collapsiblehis");
 var i;
 
 for (i = 0; i < coll.length; i++) {
