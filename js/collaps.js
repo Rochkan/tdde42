45a3ac012f2061e5f4e383a17d4c98841f44eefc
LinaBillman <lina.billman@telia.com>
2019-03-09 12:00:19 +0100

    ojojoj

--- js/collaps.js
+++ js/collaps.js
@@ -16,12 +16,12 @@ for (i = 0; i < coll.length; i++) {
 }
 
 
-var coll = document.getElementsByClassName("collapsiblenat");
+var coll = document.getElementById("collapsiblenat");
 var i;
 
 for (i = 0; i < coll.length; i++) {
   coll[i].addEventListener("click", function() {
-    this.classList.toggle("active");
+    this.IdList.toggle("active");
     var content = this.nextElementSibling;
     if (content.style.display === "block") {
       content.style.display = "none";
